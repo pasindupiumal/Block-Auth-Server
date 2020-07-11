50cba8efdf5e83b61c097893e0c8d8c77a936c13
pasindu_it17003674 <it17003674@my.sliit.lk>
2020-07-11 13:11:27 +0530

    KeyController implementation

---
+++ controllers/keyController.js
@@ -0,0 +1,16 @@
+const express =  require('express');
+const router = express.Router();
+const KeyService = require('../services/keyService');
+
+//End point for generating new sets of keys for new users
+router.get('/', (req, res) => {
+
+    KeyService.newKeys().then(data => {
+
+        res.status(data.status).send({message: data.message, data: data.data});
+
+    }).catch(error => {
+
+        res.status(error.status).send({message: error.message});
+    });
+})
