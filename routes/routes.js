5788e2f1b0acf70ecce06463776d05939dd59bac
pasindu_it17003674 <it17003674@my.sliit.lk>
2020-07-11 13:19:42 +0530

    Route implementation

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const indexRouter = require('../controllers/index');
+const indexRouter = require('../controllers/indexController');
+const keyRouter = require('../controllers/keyController');
 
 router.use('/', indexRouter);
+router.use('/keys', keyRouter);
 
 
 module.exports = router;
